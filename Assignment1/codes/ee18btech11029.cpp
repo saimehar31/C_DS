d5306de4f38411ded83ac23711aa34d28b1d3ae3
saimehar31 <47539012+saimehar31@users.noreply.github.com>
2021-09-05 11:51:25 +0530

    Add files via upload

--- Assignment1/codes/ee18btech11029.cpp
+++ Assignment1/codes/ee18btech11029.cpp
@@ -3,19 +3,6 @@ using namespace std;
 #define int long long
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-void convertBinaryToDecimal(long long n)
-{
-    int decimalNumber = 0, i = 0, remainder;
-    while (n != 0)
-    {
-        remainder = n % 10;
-        n /= 10;
-        decimalNumber += remainder * pow(2, i);
-        ++i;
-    }
-    cout << decimalNumber;
-}
-
 
 void twocomplement(int binary[])
 {
